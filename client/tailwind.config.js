f5ac6e505a31cf273f4277c5715e7a8c2ab3c4c5
KaranRaul <karanraul02@gmail.com>
2024-12-14 14:08:07 +0530

    tailwind css file changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,10 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#3b82f6",
-        danger: "#ef4444",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
